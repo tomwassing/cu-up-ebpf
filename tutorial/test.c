d3bbbb4fa99b0028c5f5b3f49695ebf661eb1b82
Tom J. Wassing <tjwassing@gmail.com>
2023-11-15 14:58:56 +0000

    added mock packet

--- tutorial/test.c
+++ tutorial/test.c
@@ -12,22 +12,27 @@
 
 
 int main (int argc, char *argv[]) {
-        struct __sk_buff skb = {0};
-        struct hello_bpf *skel;
         int prog_fd, err = 0;
+
+        char v4_pkt[(sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct tcphdr))];
+        // struct __sk_buff skb = {0};
 
         // define our BPF_PROG_RUN options with our mock data.
         struct bpf_test_run_opts opts = {
                 // required, or else bpf_prog_test_run_opts will fail
                 .sz = sizeof(struct bpf_test_run_opts),
+                // data_in will wind up being ctx.data
+                .data_in = &v4_pkt,
+                .data_size_in = sizeof(v4_pkt),
                 // ctx is an skb in this case
-                .ctx_in = &skb,
-                .ctx_size_in = sizeof(skb),
+                // .ctx_in = &skb,
+                // .ctx_size_in = sizeof(skb),
                 .repeat = 1
         };
 
         // load our fib lookup test program into the Kernel and return our
         // skeleton handle to it.
+        struct hello_bpf *skel;
         skel = hello_bpf__open_and_load();
         if (!skel) {
                 printf("[error]: failed to open and load skeleton: %d\n", err);
